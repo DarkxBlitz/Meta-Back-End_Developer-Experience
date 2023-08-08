2d0a003be0bc77c6a1516b1461e4f5c4966c8d48
William <wrhiltz@buffalo.edu>
2023-08-07 22:41:21 -0400

    another python exercise begin pushed

--- cert2/playground_rand/helloworld.py
+++ cert2/playground_rand/helloworld.py
@@ -1,6 +1,10 @@
-import random
-f = open ("newfile.txt", "r")
-f_content = f.read()
+#import random
+#f = open ("newfile.txt", "r")
+#f_content = f.read()
 #print(f_content)
-f_content_split = f_content.split("\n")
-print(random.choice(f_content_split))
+#f_content_split = f_content.split("\n")
+#print(random.choice(f_content_split))
+
+reverse_str = "reversethisstring"
+reverse_str = reverse_str[::-1]
+print(reverse_str)
